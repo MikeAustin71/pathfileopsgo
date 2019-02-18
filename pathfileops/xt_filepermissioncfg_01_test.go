fe315616e04291073ce85ad43f3e720feeb1e255
Mike Rapp <mike@paladinacs.net>
2019-02-17 22:48:35 -0600

    Added tests for FilePermissionConfig.CopyOut()

--- pathfileops/xt_filepermissioncfg_01_test.go
+++ pathfileops/xt_filepermissioncfg_01_test.go
@@ -72,6 +72,306 @@ func TestFilePermissionConfig_CopyIn_02(t *testing.T) {
 
 }
 
+func TestFilePermissionConfig_CopyOut_01(t *testing.T) {
+
+	textCode := "-rwxrwxrwx"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_02(t *testing.T) {
+
+	textCode := "drwxrwxrwx"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_03(t *testing.T) {
+
+	textCode := "-rw-rw-rw-"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_04(t *testing.T) {
+
+	textCode := "drw-rw-rw-"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_05(t *testing.T) {
+
+	textCode := "-r--r--r--"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_06(t *testing.T) {
+
+	textCode := "dr--r--r--"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_07(t *testing.T) {
+
+	textCode := "--w--w--w-"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_08(t *testing.T) {
+
+	textCode := "d-w--w--w-"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_09(t *testing.T) {
+
+	textCode := "---xr----x"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
+func TestFilePermissionConfig_CopyOut_10(t *testing.T) {
+
+	textCode := "d--xr----x"
+
+	fpCfg, err := FilePermissionConfig{}.New(textCode)
+
+	if err != nil {
+		t.Errorf("Error returned by FilePermissionConfig{}.New(textCode). "+
+			"textCode='%v' Error='%v'", textCode, err.Error())
+	}
+
+	fpCfg2 := fpCfg.CopyOut()
+
+	actualTextCode, err := fpCfg2.GetPermissionTextCode()
+
+	if err != nil {
+		t.Errorf("Error returned by fpCfg2.GetPermissionTextCode(). "+
+			"Error='%v'", err.Error())
+	}
+
+	if textCode != actualTextCode {
+		t.Errorf("Error: Expected text code ='%v'. Instead, text code='%v'. ",
+			textCode, actualTextCode)
+	}
+
+	if !fpCfg.Equal(fpCfg2) {
+		t.Error("Error: Expected fpCfg to EQUAL fpCfg2. THEY ARE NOT EQUAL!")
+	}
+}
+
 func TestFilePermissionConfig_GetEntryTypeComponent_01(t *testing.T) {
 
 	textCode := "drwxrwxrwx"
